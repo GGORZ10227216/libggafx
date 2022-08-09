9537e4f9d76dc56a900bdb74b8c0d609547edcfa
0rzgg <ab167943@gmail.com>
2022-08-10 01:00:37 +0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include <videodump_viewer.h>
+#include <gg_palette_viewer.h>
+#include <ImFileDialog.h>
+
+#define SCREEN_WIDTH  1280
+#define SCREEN_HEIGHT 768
+
+int main(int argv, char** args) {
+    using PaletteViewer = gg_widget::gg_palette_viewer;
+    gg_widget::gg_videodump_viewer<gg_widget::SDL_GL_interface> interface(SCREEN_WIDTH, SCREEN_HEIGHT) ;
+
+    interface.StartUI();
+
+    return 0;
+} // main()
